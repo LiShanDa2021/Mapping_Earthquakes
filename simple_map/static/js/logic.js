6f6e696419875205cad90e6a6f0f9da9535c3d32
Al <87392984+LiShanDa2021@users.noreply.github.com>
2021-12-05 14:31:01 -0600

    initial commit for branch

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -6,7 +6,7 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // create tile layer for the background
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
